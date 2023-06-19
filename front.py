779c9f64f4f7404215b737a938a8f05a780caadb
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2023-06-19 04:36:26 -0300

    Update front.py

--- front.py
+++ front.py
@@ -1,27 +1,29 @@
-from flask import Flask, render_template_string
 
-app = Flask(__name__)
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-@app.route('/')
-def display_variable():
-    my_variable = "Hello, World!"
-    template = "<h1>{{ variable }}</h1>"
-    return render_template_string(template, variable=my_variable)
+# Set the port number for the server
+port = 8000
 
-if __name__ == '__main__':
-    app.run()
+# Define the directory containing your frontend files
+directory = '.'  # Replace '.' with the directory path containing your frontend files
 
-# from http.server import SimpleHTTPRequestHandler, HTTPServer
+# Create an HTTP server with the SimpleHTTPRequestHandler
+httpd = HTTPServer(('', port), SimpleHTTPRequestHandler)
 
-# # Set the port number for the server
-# port = 8000
+# Start the server
+print(f'Starting server on port {port}...')
+httpd.serve_forever()
 
-# # Define the directory containing your frontend files
-# directory = '.'  # Replace '.' with the directory path containing your frontend files
 
-# # Create an HTTP server with the SimpleHTTPRequestHandler
-# httpd = HTTPServer(('', port), SimpleHTTPRequestHandler)
+# from flask import Flask, render_template_string
 
-# # Start the server
-# print(f'Starting server on port {port}...')
-# httpd.serve_forever()
+# app = Flask(__name__)
+
+# @app.route('/')
+# def display_variable():
+#     my_variable = "Hello, World!"
+#     template = "<h1>{{ variable }}</h1>"
+#     return render_template_string(template, variable=my_variable)
+
+# if __name__ == '__main__':
+#     app.run()
